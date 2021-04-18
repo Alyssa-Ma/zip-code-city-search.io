0fc23da7b4a4789a23dcceaf8bd17863156df955
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-18 19:42:20 -0400

    testing

--- src/components/IMG.js
+++ src/components/IMG.js
@@ -4,8 +4,52 @@ class IMG extends React.Component {
     render(){
         const animal = this.props.animal;
 
+        let fact = '';
+        let img_url = '';
+
+
+        
+        if(animal === "Cat")
+        {
+            fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
+            .then((res) => res.json())
+            .then((body) =>{
+                console.log(body.text);
+                fact = body.text;
+                console.log(fact);
+            })
+            .catch((e) => console.log(e));
+
+            /*
+            fetch('https://thatcopy.pw/catapi/rest/')
+            .then((res) => res.json())
+            .then((body) =>{
+                img_url = body.url;
+            }) ; */
+            
+            
+        }
+        //if animal is dog
+        else
+        {
+            //random dog fact
+            fact = fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1')
+            .then((res) => res.json()).text;
+            //random dog pic
+            img_url = fetch('https://dog.ceo/api/breeds/image/random')
+            .then((res) => res.json).url;
+        }
+
+        console.log(fact);
+        //console.log(img_url);
+        //<img src={img_url} />
         return (
-            <p>{animal}</p>
+            <div>
+                
+                <p>{fact}</p>
+                <p>Hello </p>
+            </div>
+            
         )
     } 
 }
